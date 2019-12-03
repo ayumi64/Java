91530ed4ea5fd7b8d637e96bd0e87a6d0cbdcbd0
yuhang.gyh <yuhang.gyh@alibaba-inc.com>
2019-12-03 21:30:44 +0800

    1
    1

---
+++ learn/variable/Employ.java
@@ -0,0 +1,13 @@
+
+public class Employ {
+    private static double salary; //静态私有变量
+
+    public static final String DEPAT = "开发";
+    public static void main(String[] args) {
+        salary = 10000;
+            System.out.println(DEPAT+"salary is:" + salary);
+    }
+	public static void DEPAT() {
+	}
+
+}
